67cd4afd193b1c3bd77cfc8a08602340880449db
feliciaannabelruri <163542970+feliciaannabelruri@users.noreply.github.com>
2024-05-04 09:20:18 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Hotels from "./pages/Hotels";
+import Game from "./pages/Game";\
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/hotels" element={<Hotels />} />
+        <Route path="/game" element={<Game/>} />
       </Routes>
     </Router>
   );
