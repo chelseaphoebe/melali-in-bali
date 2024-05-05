29c981e3f5f5791756d790580b6850e10b2fda5e
feliciaannabelruri <163542970+feliciaannabelruri@users.noreply.github.com>
2024-05-05 18:24:10 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
 		</a>
 		<div className="flex gap-14">
 		  <a href="/hotels" className="nav-link">Book Hotels</a>
-		  <a href="#" className="nav-link-placeholder">Inspirasi Seru</a>
+		  <a href="/inspirasi">Inspirasi Seru</a>
 		  <a href="#" className="nav-link-placeholder">Rencanakan Perjalanan</a>
 		  <a href="/game" className="nav-link">Game</a>
 		  <a href="#" className="nav-link-placeholder">About Us</a>
