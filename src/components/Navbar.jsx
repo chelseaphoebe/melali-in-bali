bbd7d8c085e639ccf13dffedb0060f4e0be902e9
Nevanoz <163624599+Nevanoz@users.noreply.github.com>
2024-05-05 13:51:09 +0700

    we

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
 	return (
 	  <div className="bg-black text-white pl-14 pr-28 flex justify-between items-center bg-opacity-75 fixed w-full py-3">
-		<a href="/">
+		<a href="/" className="logo-link">
 		  <img src="./images/logo.png" alt="" className="size-12" />
 		</a>
 		<div className="flex gap-14">
@@ -9,8 +9,7 @@ export default function Navbar() {
 		  <a href="#" className="nav-link-placeholder">Inspirasi Seru</a>
 		  <a href="#" className="nav-link-placeholder">Rencanakan Perjalanan</a>
 		  <a href="/game" className="nav-link">Game</a>
-		  <a href="#" className="nav-link-placeholder">Go Explore</a>
-		  <a href="#" className="nav-link-placeholder">Panduan Dasar</a>
+		  <a href="#" className="nav-link-placeholder">About Us</a>
 		</div>
 	  </div>
 	);
