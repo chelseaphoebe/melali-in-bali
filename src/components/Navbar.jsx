169eef9f680bd9b1358993df7801875a27172b76
Nevanoz <163624599+Nevanoz@users.noreply.github.com>
2024-05-05 12:42:11 +0700

    navabar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,18 @@
 export default function Navbar() {
 	return (
-		<div className="bg-black text-white pl-14 pr-28  flex justify-between items-center bg-opacity-75 fixed w-full py-3">
-			<a href="/"> 
-        <img src="./images/logo.png" alt="" className="size-12"/>
-      </a>
-			<div className="flex gap-14">
-				<a href="/hotels">Book Hotels</a>
-				<button>Inspirasi Seru</button>
-				<button>Rencanakan Perjalanan</button>
-				<a href="/game">Game</a>
-				<button>Go Explore</button>
-				<button>Panduan Dasar</button>
-			</div>
+	  <div className="bg-black text-white pl-14 pr-28 flex justify-between items-center bg-opacity-75 fixed w-full py-3">
+		<a href="/">
+		  <img src="./images/logo.png" alt="" className="size-12" />
+		</a>
+		<div className="flex gap-14">
+		  <a href="/hotels" className="nav-link">Book Hotels</a>
+		  <a href="#" className="nav-link-placeholder">Inspirasi Seru</a>
+		  <a href="#" className="nav-link-placeholder">Rencanakan Perjalanan</a>
+		  <a href="/game" className="nav-link">Game</a>
+		  <a href="#" className="nav-link-placeholder">Go Explore</a>
+		  <a href="#" className="nav-link-placeholder">Panduan Dasar</a>
 		</div>
+	  </div>
 	);
-}
+  }
+  
