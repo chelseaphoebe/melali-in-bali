401a6a434aba7ef3f5a0641b8a7472f75052904f
Nevanoz <163624599+Nevanoz@users.noreply.github.com>
2024-05-05 19:29:46 +0700

    haha

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import './Navbar.css';
+
 export default function Navbar() {
-	return (
-	  <div className="bg-black text-white pl-14 pr-28 flex justify-between items-center bg-opacity-75 fixed w-full py-3">
-		<a href="/" className="logo-link">
-		  <img src="./images/logo.png" alt="" className="size-12" />
-		</a>
-		<div className="flex gap-14">
-		  <a href="/hotels" className="nav-link">Book Hotels</a>
-		  <a href="/inspirasi">Inspirasi Seru</a>
-		  <a href="#" className="nav-link-placeholder">Rencanakan Perjalanan</a>
-		  <a href="#" className="nav-link-placeholder">Inspirasi Seru</a>
-		  <a href="/game" className="nav-link">Game</a>
-		  <a href="/aboutUs" className="nav-link-placeholder">Tentang Kami</a>
-		</div>
-	  </div>
-	);
-  }
-  
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+
+      
+      if (scrollPosition > 100) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false); 
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className={`navbar ${isScrolled ? 'scrolled' : ''}`}>
+      <a href="/" className="logo-link">
+        <img src="./images/logo.png" alt="" className="size-12" />
+      </a>
+      <div className="nav-links">
+        <a href="/hotels" className="nav-link">Book Hotels</a>
+        <a href="#" className="nav-link">Rencanakan Perjalanan</a>
+		<a href="/inspirasi" className="nav-link">Inspirasi Seru</a>
+        <a href="/game" className="nav-link">Game</a>
+        <a href="/aboutUs" className="nav-link">Tentang Kami</a>
+      </div>
+    </div>
+  );
+}
