ea92636ac2081e93716450ccfb4cb08c8ea0601c
ChelseaArantza <chelsearantza123456@gmail.com>
2024-05-05 23:04:21 +0700

    fix: Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,30 +3,42 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 export default function Navbar() {
+  const [topPage, setTopPage] = useState("");
 
-	const [topPage, setTopPage] = useState('') 
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY === 0) {
+        setTopPage("bg-opacity-0 text-white");
+      } else {
+        setTopPage("bg-white bg-opacity-100 text-black");
+      }
+    });
+  }, []);
 
-	useEffect (() => {
-		window.addEventListener('scroll', ()=> {
-			if(window.scrollY === 0){
-				setTopPage("bg-opacity-0 text-white");
-			} else {
-				setTopPage("bg-white bg-opacity-100 text-black");
-			}
-		})}, []
-	);
-
-	return (
-		<div id="navBar" className={`${topPage} text-white pl-14 pr-28  flex justify-between items-center bg-opacity-75 fixed w-full py-3`}>
-			<a href="/"> 
-        <img src="./images/logo.png" alt="" className="size-12"/>
+  return (
+    <div
+      id="navBar"
+      className={`${topPage} text-white pl-14 pr-28 flex justify-between items-center bg-opacity-75 fixed w-full py-3`}
+    >
+      <a href="/">
+        <img src="./images/logo.png" alt="" className="size-12" />
       </a>
-      <div className="nav-links">
-        <a href="/hotels" className="nav-link">Book Hotels</a>
-        <a href="#" className="nav-link">Rencanakan Perjalanan</a>
-		<a href="/inspirasi" className="nav-link">Inspirasi Seru</a>
-        <a href="/game" className="nav-link">Game</a>
-        <a href="/aboutUs" className="nav-link">Tentang Kami</a>
+      <div className="nav-links w-2/4 flex justify-between">
+        <a href="/hotels" className="nav-link">
+          Book Hotels
+        </a>
+        <a href="#section2" className="nav-link">
+          Rencanakan Perjalanan
+        </a>
+        <a href="/inspirasi" className="nav-link">
+          Inspirasi Seru
+        </a>
+        <a href="/game" className="nav-link">
+          Game
+        </a>
+        <a href="/aboutUs" className="nav-link">
+          Tentang Kami
+        </a>
       </div>
     </div>
   );
