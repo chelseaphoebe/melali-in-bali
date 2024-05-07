ac1dd6dd7c6edba34e7c2e44c5ca56cac9e2f9f7
Nevanoz <163624599+Nevanoz@users.noreply.github.com>
2024-05-07 12:04:05 +0700

    Update Game.jsx

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -7,7 +7,7 @@ import Swal from 'sweetalert2';
 const gridSize = 20;
 const gridCount = 20;
 
-function Navbar({ onStartGame }) { // Destructuring onStartGame from props
+function Navbar({ onStartGame }) { 
   const handleStartGame = () => {
     if (onStartGame) {
       onStartGame();
