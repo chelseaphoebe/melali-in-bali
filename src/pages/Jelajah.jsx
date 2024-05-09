bd5c0b8b281d309163ac9af179bb91dddbd73ad2
ChelseaArantza <chelsearantza123456@gmail.com>
2024-05-09 10:58:51 +0700

    update: Jelajah.csx

--- src/pages/Jelajah.jsx
+++ src/pages/Jelajah.jsx
@@ -1,18 +1,9 @@
-import React from "react";
-import "./Jelajah.css";
+import JelajahCard from "../components/JelajahCard"
 
-function Card (){
-    return(
-        <div className="card">
-            <img src="../images/background3.jpg" className="card-img" alt="#"/>
-            <div className="card-body">        
-            <h1 className="card-title">JUDUL</h1>
-            <p className="card-sub-title">sub judul</p>    
-            <p className="card-info">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla quis sapiente magnam nemo error nam incidunt ipsa magni beatae inventore.</p>    
-            <button className="card-btn">CLICK HERE</button> 
-            </div>
-        </div>
-    )
+function Jelajah() {
+  return (
+    <JelajahCard judul="aku tatang" subjudul="aku suka sisdig" desc="duar nmax" link=""/>
+  )
 }
 
-export default Card;
+export default Jelajah;
