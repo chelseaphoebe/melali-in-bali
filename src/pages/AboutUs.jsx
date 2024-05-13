2abdbd5bf68e7b47d7cb4ab09f937fac23231c4b
ChelseaArantza <chelsearantza123456@gmail.com>
2024-05-14 02:12:59 +0700

    fix: ongoing aboutUS

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -41,8 +41,8 @@ const AboutUs = () => {
                 Austin Gilbert Liwanto adalah seorang mahasiswa semester 2
                 jurusan Informatika di Universitas Multimedia Nusantara. Dia
                 merupakan orang yang penuh semangat dan ketertarikan dalam
-                menjelajahi pengalaman baru baik itu hal akademik
-                maupun non-akademik.
+                menjelajahi pengalaman baru baik itu hal akademik maupun
+                non-akademik.
               </p>
               <div className="flex gap-6 md:gap-4 mt-4">
                 <a
@@ -60,16 +60,18 @@ const AboutUs = () => {
           </div>
         </div>
         <div className="flex min-h-[450px] max-w-[380px] justify-between overflow-hidden rounded-3xl">
-          <div className="bg-[url('/public/images/austin.png')] bg-cover flex items-end">
+          <div className="bg-[url('/public/images/chelsea.jpeg')] bg-cover flex items-end">
             <div className="bg-gradient-to-b from-[#091426]/0 to-[#091426] p-4">
               <p className="text-3xl text-white">Chelsea</p>
               <p className="text-xl font-semibold text-white">00000096816</p>
               <p className="text-sm font-normal text-white">
-                Austin Gilbert Liwanto adalah seorang mahasiswa semester 2
-                jurusan Informatika di Universitas Multimedia Nusantara. Dia
-                merupakan orang yang penuh semangat dan ketertarikan dalam
-                menjelajahi pengalaman baru baik itu hal akademik
-                maupun non-akademik.
+                Chelsea Arantza Phoebe adalah seorang mahasiswa semester 2 di
+                Universitas Multimedia Nusantara, jurusan Informatika. Dia
+                adalah seorang perempuan yang berbakat dalam bermain musik dan
+                memiliki ketertarikan yang besar dalam pembuatan web.
+                Kreativitasnya tak terbatas, dan dia selalu bersemangat untuk
+                menggabungkan kecintaannya pada musik dengan keahliannya dalam
+                teknologi untuk menciptakan web yang menarik dan inovatif.
               </p>
               <div className="flex gap-6 md:gap-4 mt-4">
                 <a
@@ -78,16 +80,6 @@ const AboutUs = () => {
                 >
                   <Icon
                     icon="mdi:instagram"
-                    className="w-[30px] h-[30px]"
-                    style={{ color: "white" }}
-                  />
-                </a>
-                <a
-                  href="https://www.instagram.com/chelseaarantza/"
-                  target="_blank"
-                >
-                  <Icon
-                    icon="mdi:twitter"
                     className="w-[30px] h-[30px]"
                     style={{ color: "white" }}
                   />
@@ -117,43 +109,40 @@ const AboutUs = () => {
           </div>
         </div>
         <div className="flex min-h-[450px] max-w-[380px] justify-between overflow-hidden rounded-3xl">
-          <div className="bg-[url('/public/images/austin.png')] bg-cover flex items-end">
+          <div className="bg-[url('/public/images/feli.png')] bg-cover flex items-end">
             <div className="bg-gradient-to-b from-[#091426]/0 to-[#091426] p-4">
-              <p className="text-3xl text-white">Austin</p>
-              <p className="text-xl font-semibold text-white">00000096252</p>
+              <p className="text-3xl text-white">Felicia</p>
+              <p className="text-xl font-semibold text-white">00000096911</p>
               <p className="text-sm font-normal text-white">
-                Austin Gilbert Liwanto adalah seorang mahasiswa semester 2
-                jurusan Informatika di Universitas Multimedia Nusantara. Dia
-                merupakan orang yang penuh semangat dan ketertarikan dalam
-                menjelajahi pengalaman baru baik itu hal akademik
-                maupun non-akademik.
+                Felicia Annabel adalah seorang mahasiswi semester 2 jurusan
+                Informatika di Universitas Multimedia Nusantara, di dalam waktu
+                yang bersamaan menjalani karirnya dalam Marketing yaitu Social
+                Media Specialist. Dia orang yang suka bekerja dan
+                melakukan petualangan.
               </p>
               <div className="flex gap-6 md:gap-4 mt-4">
-                <a
-                  href="https://www.instagram.com/austingilbert.l/"
-                  target="_blank"
-                >
+                <a href="https://www.instagram.com/flciar/" target="_blank">
                   <Icon
                     icon="mdi:instagram"
                     className="w-[30px] h-[30px]"
                     style={{ color: "white" }}
                   />
                 </a>
-                <a href="https://www.instagram.com/austingilbert.l/">
+                <a href="https://www.instagram.com/flciar/">
                   <Icon
                     icon="mdi:twitter"
                     className="w-[30px] h-[30px]"
                     style={{ color: "white" }}
                   />
                 </a>
-                <a href="https://www.instagram.com/austingilbert.l/">
+                <a href="https://www.instagram.com/flciar/">
                   <Icon
                     icon="mdi:youtube"
                     className="w-[30px] h-[30px]"
                     style={{ color: "white" }}
                   />
                 </a>
-                <a href="https://www.instagram.com/austingilbert.l/">
+                <a href="https://www.instagram.com/flciar/">
                   <Icon
                     icon="ic:baseline-tiktok"
                     className="w-[30px] h-[30px]"
@@ -165,16 +154,14 @@ const AboutUs = () => {
           </div>
         </div>
         <div className="flex min-h-[450px] max-w-[380px] justify-between overflow-hidden rounded-3xl">
-          <div className="bg-[url('/public/images/austin.png')] bg-cover flex items-end">
+          <div className="bg-[url('/public/images/josh.png')] bg-cover flex items-end">
             <div className="bg-gradient-to-b from-[#091426]/0 to-[#091426] p-4">
-              <p className="text-3xl text-white">Austin</p>
-              <p className="text-xl font-semibold text-white">00000096252</p>
+              <p className="text-3xl text-white">Joshua</p>
+              <p className="text-xl font-semibold text-white">00000094196</p>
               <p className="text-sm font-normal text-white">
-                Austin Gilbert Liwanto adalah seorang mahasiswa semester 2
-                jurusan Informatika di Universitas Multimedia Nusantara. Dia
-                merupakan orang yang penuh semangat dan ketertarikan dalam
-                menjelajahi pengalaman baru baik itu hal akademik
-                maupun non-akademik.
+                Joshua wijaya adalah seorang mahasiswa semester 2 jurusan
+                Informatika di Universitas Multimedia Nusantara. Dia merupakan
+                orang yang suka berolahraga dan beraktivitas fisik.
               </p>
               <div className="flex gap-6 md:gap-4 mt-4">
                 <a
@@ -216,22 +203,6 @@ const AboutUs = () => {
 
           <div className="program">
             <div className="caption">
-              <p>Austin</p>
-              <p>00000096252</p>
-            </div>
-            <img src={card_AboutUS1} alt="Austin" />
-          </div>
-
-          <div className="program">
-            <div className="caption">
-              <p>Felicia</p>
-              <p>00000096911</p>
-            </div>
-            <img src={card_AboutUS2} alt="Feli" />
-          </div>
-
-          <div className="program">
-            <div className="caption">
               <p>Joshua Wijaya</p>
               <p>00000094196</p>
             </div>
@@ -246,21 +217,21 @@ const AboutUs = () => {
             <img src={card_AboutUS4} alt="Chelsea" />
           </div> */}
       </section>
-        <div className="carousel px-14 mt-4 ">
-          <TransitionGroup className="carousel">
-            <CSSTransition
-              key={currentImageIndex}
-              timeout={500}
-              classNames="slide"
-            >
-              <img
-                src={images[currentImageIndex]}
-                alt="Carousel Image"
-                className="carousel-image"
-              />
-            </CSSTransition>
-          </TransitionGroup>
-        </div>
+      <div className="carousel px-14 mt-4 ">
+        <TransitionGroup className="carousel">
+          <CSSTransition
+            key={currentImageIndex}
+            timeout={500}
+            classNames="slide"
+          >
+            <img
+              src={images[currentImageIndex]}
+              alt="Carousel Image"
+              className="carousel-image"
+            />
+          </CSSTransition>
+        </TransitionGroup>
+      </div>
 
       <Footer />
     </>
